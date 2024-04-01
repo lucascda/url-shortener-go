32a8ba2e2b0bfbafaeb1598134318cb8330e780c
Lucas César <43433070+lucascda@users.noreply.github.com>
2024-04-01 15:21:17 -0300

    chore: add database config

---
+++ src/database/database.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", "localhost", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to database.")
+	}
+}
--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"go-url-shortener/src/database"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -12,9 +13,12 @@ func init() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	database.ConnectToDB()
+
 }
 
 func main() {
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
