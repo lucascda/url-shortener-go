aa3d3608aef8f10e3fb3efbc8c0063938731df10
Lucas César <43433070+lucascda@users.noreply.github.com>
2024-04-06 15:30:02 -0300

    add validation to passwordConfirmation and email

--- src/models/userModel.go
+++ src/models/userModel.go
@@ -10,8 +10,8 @@ type User struct {
 }
 
 type CreateUser struct {
-	Email                string `validate:"required,`
+	Email                string `validate:"required,email"`
 	Name                 string `validate:"required"`
 	Password             string `validate:"required"`
-	PasswordConfirmation string `validate:"required"`
+	PasswordConfirmation string `validate:"required,eqfield=Password"`
 }
