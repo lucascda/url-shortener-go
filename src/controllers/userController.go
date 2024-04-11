dc42d9d529aeb36a968e899d1c0b4c58c454211c
Lucas César <43433070+lucascda@users.noreply.github.com>
2024-04-10 21:13:31 -0300

    refactor: change validation error collection to its own function

--- src/controllers/userController.go
+++ src/controllers/userController.go
@@ -50,6 +50,16 @@ func (controller *UserController) CreateUser(c *gin.Context) {
 
 }
 
+func collectErrors(err error) []common.ValidationError {
+	var validationErrors []common.ValidationError
+	for _, err := range err.(validator.ValidationErrors) {
+		validationError := common.NewValidationError(err.StructField(), err.Error(), err.Tag())
+		validationErrors = append(validationErrors, *validationError)
+	}
+
+	return validationErrors
+}
+
 func handleError(c *gin.Context, logger *zap.SugaredLogger, err error) {
 	switch {
 	case err == nil:
@@ -59,13 +69,7 @@ func handleError(c *gin.Context, logger *zap.SugaredLogger, err error) {
 		return
 	case errors.As(err, &validator.ValidationErrors{}):
 		logger.Infof("failed validation")
-		var validationErrors []common.ValidationError
-		for _, err := range err.(validator.ValidationErrors) {
-			validationError := common.NewValidationError(err.StructField(), err.Error(), err.Tag())
-
-			validationErrors = append(validationErrors, *validationError)
-
-		}
+		validationErrors := collectErrors(err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": validationErrors})
 		return
 	case errors.Is(err, apierrors.UserAlreadyExistsError{}):
